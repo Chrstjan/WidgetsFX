ad6d9886f5fa656436cf515e4689b6767a0e37a6
Chrstjan <ps5christian@hotmail.com>
2026-01-28 20:53:19 +0100

    impl: Made Decorator functions for Labels + started factory functions for the different labels

--- src/main/kotlin/io/github/chrstjan/widgetsfx/layouts/labels/Labels.kt
+++ src/main/kotlin/io/github/chrstjan/widgetsfx/layouts/labels/Labels.kt
@@ -1,5 +1,11 @@
 package io.github.chrstjan.widgetsfx.layouts.labels
 
+import io.github.chrstjan.widgetsfx.layouts.extensions.addStyle
+import javafx.beans.value.ObservableObjectValue
+import javafx.beans.value.ObservableStringValue
+import javafx.scene.Node
+import javafx.scene.control.ContentDisplay
+import javafx.scene.control.Label
 import javafx.scene.control.Labeled
 
 /**
@@ -24,3 +30,80 @@ enum class LabelStyle(val selector: String) {
  * @return [Labeled]
  */
 infix fun <T: Labeled> T.styleAs(labelStyle: LabelStyle): T = apply { styleClass += labelStyle.selector }
+
+/**
+ * Decorator function that binds a Labeled's textProperty to an external Property
+ *
+ * @param value The external ObservableStringValue to bind to the Label's Text Property
+ */
+infix fun <T: Labeled> T.bindTo(value: ObservableStringValue): T = apply { textProperty().bind(value) }
+fun <T: Labeled> T.wrapText(): T = apply { isWrapText = true }
+infix fun <T: Labeled> T.underlined(setUnderlineOn: Boolean): T = apply { isUnderline = setUnderlineOn }
+infix fun <T: Labeled> T.oriented(orientation: ContentDisplay): T = apply { contentDisplay = orientation }
+
+/**
+ * Decorator function that binds a Labeled's graphicsProperty to the specified Property
+ *
+ * @param graphicProperty ObservableObjectValue<Node> To bind to the graphicProperty
+ */
+infix fun <T: Labeled> T.bindGraphics(graphicProperty: ObservableObjectValue<Node>): T = apply { graphicProperty().bind(graphicProperty()) }
+
+/**
+ * Factory method that creates a [LabelStyle.PROMPT] Label with a textProperty bound to an external Property
+ *
+ * @param value The external Property to bind to the Text property
+ */
+fun promptOf(value: ObservableStringValue) = Label() styleAs LabelStyle.PROMPT bindTo value
+
+/**
+ * Factory method that creates a [LabelStyle.PROMPT] Label with a provided static Text value
+ *
+ * @param String The Text Value for the Label
+ */
+fun promptOf(value: String) = Label(value) styleAs LabelStyle.PROMPT
+
+/**
+ * Factory method that creates a Label with a bound textProperty to an external property
+ *
+ * @param value The external ObservableStringValue to bind to the Label's Text Property
+ */
+fun labelOf(value: ObservableStringValue) = Label() bindTo value
+
+/**
+ * Factory method that creates a Label with a bound textProperty to an external property,
+ * Along with a styleClass selector and with an optional set Graphic value
+ *
+ * @param value The external ObservableStringValue to bind to the Label's Text Property
+ * @param styleClass String to use as style class selector
+ * @param graphicNode Optional: Node used as the Label's Graphic property
+ */
+fun labelOf(value: ObservableStringValue, styleClass: String, graphicNode: Node? = null) = Label().apply {
+    graphicNode?.let { graphic = it }
+} bindTo value addStyle styleClass
+
+/**
+ * Factory method that creates a Label with a bound textProperty to an external property,
+ * Along with a styleClass selector and the Graphic property bound to an ObservableObjectValue<Node>
+ *
+ * @param value The external ObservableStringValue to bind to the Label's Text Property
+ * @param styleClass String to use as style class selector
+ * @param graphicProperty ObservableObjectValue<Node> To bind to the graphicProperty
+ */
+fun labelOf(value: ObservableStringValue, styleClass: String, graphicProperty: ObservableObjectValue<Node>) =
+    Label() bindTo value bindGraphics graphicProperty addStyle styleClass
+
+/**
+ * Factory method that creates a [LabelStyle.H1] styled [Label] with static text
+ *
+ * @param value The static text for the Label
+ * @return [Label]
+ */
+fun h1Of(value: String, graphic: Node? = null) = Label(value, graphic) styleAs LabelStyle.H1
+
+/**
+ * Factory method that creates a [LabelStyle.H1] styled [Label] with a bound text property
+ *
+ * @param value The Label's bound external ObservableStringProperty
+ * @return [Label]
+ */
+fun h1Of(value: ObservableStringValue, graphic: Node? = null) = Label("", graphic) styleAs LabelStyle.H1 bindTo value
